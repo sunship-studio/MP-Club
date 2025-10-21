28e0f46ff2c0dd9d6a368ecde3b73ace8de7633a
Igor Kamrowski <kamryydev@gmail.com>
2025-10-21 14:37:02 +0200

    upload

---
+++ mpc-front/next.config.ts
@@ -0,0 +1,34 @@
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://checkout.stripe.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://checkout.stripe.com",
+              "frame-src https://js.stripe.com https://checkout.stripe.com https://hooks.stripe.com",
+              "connect-src 'self' https://api.stripe.com https://checkout.stripe.com https://m.stripe.com",
+              "img-src 'self' data: https: https://*.stripe.com",
+              "font-src 'self' https://fonts.gstatic.com data:",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
