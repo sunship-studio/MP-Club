dd2d72dc470d3acd2692b358a1ffa35f210fbcaf
Igor Kamrowski <kamryydev@gmail.com>
2025-10-21 14:40:04 +0200

    update

--- mpc-front/next.config.ts
+++ mpc-front/next.config.ts
@@ -10,11 +10,11 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value: [
-              "default-src 'self'",
+ "default-src 'self'",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://checkout.stripe.com",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://checkout.stripe.com",
               "frame-src https://js.stripe.com https://checkout.stripe.com https://hooks.stripe.com",
-              "connect-src 'self' https://api.stripe.com https://checkout.stripe.com https://m.stripe.com",
+              "connect-src 'self' https://mp-club-production.up.railway.app https://api.stripe.com https://checkout.stripe.com https://m.stripe.com",
               "img-src 'self' data: https: https://*.stripe.com",
               "font-src 'self' https://fonts.gstatic.com data:",
             ].join('; '),
